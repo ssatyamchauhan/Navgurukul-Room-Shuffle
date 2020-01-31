5a7e39dd2656a1a7e0885e0910ef9e6b7d681769
ssatyamchauhan <ssatyamchauhan1998@gmail.com>
2020-01-31 09:54:15 +0530

    addeed newbie students in the list

--- roomshuffle.js
+++ roomshuffle.js
@@ -1,15 +1,14 @@
-var fs = require('fs');
+let r1 = ['Nitish', 'Biju', 'Kirithiv']
+let r6 = ['Kartik', 'Praveen', 'Shakti', 'Sonu', 'Shubham', 'Aman k', 'Bijendra']
+let r7 = ['V nayak', 'Bhavnesh', 'Rahit', 'Bhaskar', 'R L Yogessh', 'Deepak sh', 'Paramthaman', 'Dipesh']
+let r8 = ['Aakash', 'Prince', 'Yousuf', 'J Prakash', 'V modi', 'Bilal']
+let r9 = ['Sanjay', 'Shabid', 'Ajith', 'Atul']
+let r10 = ['Rohit', 'Roshan', 'Riyaz', 'Hemant', 'Tapas', 'Rakesh', 'Akhilesh', 'Ramesh']
+let r11 = ['Alpesh', 'Kishore', 'Prakash', 'Prabhakar', 'Himanshu', 'Anand']
+let r12 = ['Ankur', 'Sushant', 'Santosh', 'Tariq A', 'Umesh', 'Amar pal', 'Abhishek', 'Chandan sh', 'S Vivek']
 
-var r6 = ['Pratik', 'Bhavnesh', 'Biju', 'Akash', 'Ramesh', 'Tariq', 'Deepak']
-var r7 = ['Santosh', 'Mohit', 'Vivek', 'Tarun', 'Chandrashekhar', 'Raushan', 'Dipesh']
-var r8 = ['Aman', 'S Vivek', 'Prabhakar', 'Bhaskar', 'Chandan']
-var r9 = ['Aman Sharma', 'Kishor']
-var r10 = ['Yogessh', 'Alpesh', 'Rahit', 'Jaiprakash', 'Shabid', 'Kirithiv']
-var r11 = ['Atul', 'Shahid', 'Ajith', 'Hemant', 'Riyaz', 'Rakesh']
-var r12 = ['Anand', 'Sanjay', 'K Nayak', 'Vishal', 'Kapil', 'Himanshu', 'Paranthaman', 'Akhilesh', 'Rohit']
-
-var allRooms = [r6, r7, r8, r9, r10, r11, r12]
-var Room = [6, 7, 8, 9, 10, 11, 12]
+var allRooms = [r1 ,r6, r7, r8, r9, r10, r11, r12]
+var Room = [1, 6, 7, 8, 9, 10, 11, 12]
 
 var listofDetailedStudent = []
 var i = 0;
@@ -25,13 +24,14 @@ for (var room of allRooms) {
 }
 
 var roomAndBed = {
-    6: [1, 2, 3, 4, 5, 6, 7],
-    7: [8, 9, 10, 11, 12, 13, 14, 15],
-    8: [16, 17, 18, 19, 20, 21],
-    9: [22, 23, 24, 25],
-    10: [26, 27, 28, 29, 30, 31, 32, 33],
-    11: [34, 35, 36, 37, 38, 39],
-    12: [40, 41, 42, 43, 44, 45, 46, 47, 48, 49]
+    1: [1, 2, 3, 4, 5, 6, 7, 8],
+    6: [10, 11, 12, 13, 14, 15, 16],
+    7: [17, 18, 19, 20, 21, 22, 23, 24],
+    8: [25, 26, 27, 28, 29, 30],
+    9: [31, 32, 33, 34],
+    10: [35, 36, 37, 38, 39, 40, 41, 42],
+    11: [43, 44, 45, 46, 47, 48],
+    12: [49, 50, 51, 52, 53, 54, 55, 56]
 }
 
 var assignedStudent = [];
@@ -63,7 +63,13 @@ module.exports = (endpoints) => {
                 }
             }
         }
-        console.log(shuffleDone)
+        let newSet = new Set();
+        shuffleDone.map((i) => {
+            newSet.add(i.name)
+        })
+        if(newSet.size === shuffleDone.length){
+            console.log(true)
+        }
             return res.json(shuffleDone)
     })
 }
